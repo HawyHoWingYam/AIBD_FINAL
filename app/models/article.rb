206ba7092f1820eae4a5cd26bd6bc8f6b7c9b806
karasho62 <karasho62@gmail.com>
2023-03-21 18:01:37 +0800

    Stop at Refactoring

---
+++ app/models/article.rb
@@ -0,0 +1,7 @@
+class Article < ApplicationRecord
+  has_many :comments
+  has_many :posts
+
+  validates :title, presence: true
+  validates :body, presence: true, length: { minimum: 10 }
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,35 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_03_20_045740) do
-  create_table "posts", force: :cascade do |t|
+ActiveRecord::Schema[7.0].define(version: 2023_03_21_081839) do
+  create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "status"
   end
 
+  create_table "comments", force: :cascade do |t|
+    t.string "commenter"
+    t.text "body"
+    t.integer "article_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "status"
+    t.index ["article_id"], name: "index_comments_on_article_id"
+  end
+
+  create_table "posts", force: :cascade do |t|
+    t.string "poster"
+    t.text "body"
+    t.integer "article_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "status"
+    t.index ["article_id"], name: "index_posts_on_article_id"
+  end
+
+  add_foreign_key "comments", "articles"
+  add_foreign_key "posts", "articles"
 end
